cf3f57858e36d0a2d62404e093fbc561a49b9579
Keaton Burleson <keaton.burleson@me.com>
2016-07-27 11:47:49 -0500

    Fixed a shuffling crash and bug where if the URIs would become unsorted :(

--- Simpilfy/PlayController.swift
+++ Simpilfy/PlayController.swift
@@ -10,111 +10,130 @@ import Foundation
 import UIKit
 import MediaPlayer
 
-class PlayController: NSObject, SPTAudioStreamingDelegate{
-	
+class PlayController: NSObject, SPTAudioStreamingDelegate {
+
 	var songs: [SPTPartialTrack]? = []
-	var player: SPTAudioStreamingController?  = SPTAudioStreamingController.sharedInstance()
+	var player: SPTAudioStreamingController? = SPTAudioStreamingController.sharedInstance()
 	var session: SPTSession?
 	var auth: SPTAuth?
 	var uris: [NSURL]? = []
 
 	var isInitialized = false
-	var timeIntoSongPaused : NSTimeInterval = 0
+	var timeIntoSongPaused: NSTimeInterval = 0
 	var currentSong = 0
 	var currentPlaylistURI = NSURL()
 	var newPlaylistURI = NSURL()
-	
-	func setupAuthorization(){
+
+	var didShuffle = false
+
+	func setupAuthorization() {
 		auth = SPTAuth.defaultInstance()
-		  let rootViewController = UIApplication.sharedApplication().delegate!.window?!.rootViewController?.childViewControllers[0].childViewControllers[0] as! HomeViewController
-		self.session = rootViewController.session
-	 }
-	
-	func initWithSongs(){
-		setupAuthorization()
 		auth!.clientID = "7fedf5f10ea84f069aae21eb9e06b73b"
 		auth!.redirectURL = NSURL(string: "simplyfy://login")
 		auth!.requestedScopes = [SPTAuthStreamingScope]
-		
-		
+		let rootViewController = UIApplication.sharedApplication().delegate!.window?!.rootViewController?.childViewControllers[0].childViewControllers[0] as! HomeViewController
+		self.session = rootViewController.session
+	}
+
+	func initWithSongs() {
+		setupAuthorization()
+
 		setupiOSDefaultControls()
 		self.initializePlayerWithSession(self.session!)
 		self.beginPlaying(false)
 		self.setLockScreenData()
-	   self.setLockScreenPlayed()
-		
+		self.setLockScreenPlayed()
+
 		self.timeIntoSongPaused = self.player!.currentPlaybackPosition
-		
-		
-		
 	}
 	func initializePlayerWithSession(session: SPTSession) {
 		if (isInitialized == false && player?.loggedIn == false) {
-		self.isInitialized = true
-			do{
+			self.isInitialized = true
+			do {
 				try self.player?.startWithClientId("7fedf5f10ea84f069aae21eb9e06b73b")
-			}catch _{
+			} catch _ {
 				print("player already initalized")
 			}
-			
 		}
-		
+
 		player?.delegate = self
 		player?.loginWithAccessToken(session.accessToken)
-	
 	}
-	
-	func setupiOSDefaultControls(){
+
+	func setupiOSDefaultControls() {
 		MPRemoteCommandCenter.sharedCommandCenter().previousTrackCommand.enabled = true
 		MPRemoteCommandCenter.sharedCommandCenter().previousTrackCommand.addTarget(self, action: #selector(PlayController.previous))
-		
+
 		MPRemoteCommandCenter.sharedCommandCenter().nextTrackCommand.enabled = true
 		MPRemoteCommandCenter.sharedCommandCenter().nextTrackCommand.addTarget(self, action: #selector(PlayController.next))
-		
+
 		MPRemoteCommandCenter.sharedCommandCenter().playCommand.enabled = true
 		MPRemoteCommandCenter.sharedCommandCenter().playCommand.addTarget(self, action: #selector(PlayController.play))
-		
+
 		MPRemoteCommandCenter.sharedCommandCenter().pauseCommand.enabled = true
 		MPRemoteCommandCenter.sharedCommandCenter().pauseCommand.addTarget(self, action: #selector(PlayController.pause))
 	}
-	
+
 	func getCurrentSong() -> SPTPartialTrack {
-		return songs![currentSong%(songs?.count)!]
+		return songs![currentSong % (songs?.count)!]
 	}
-	
+	func playShuffle() {
+		if isInitialized == false {
+			setupAuthorization()
+			setupiOSDefaultControls()
+			self.initializePlayerWithSession(self.session!)
+		}
+
+		didShuffle = true
+		uris?.removeAll()
+
+		currentPlaylistURI = newPlaylistURI
+
+		for track in songs! {
+			uris?.append(track.uri)
+		}
+		uris?.shuffleInPlace()
+
+		self.setLockScreenData()
+		self.setLockScreenPlayed()
+
+		self.setLockScreenData()
+
+		player?.playURIs(uris, fromIndex: Int32(currentSong), callback: nil)
+		self.timeIntoSongPaused = self.player!.currentPlaybackPosition
+	}
+
 	func setLockScreenPaused() {
 		var dict = MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo
 		if (dict == nil) {
 			dict = [String: AnyObject]()
 		}
 		var info = dict!
-		
+
 		info[MPNowPlayingInfoPropertyPlaybackRate] = 0.0
 		info[MPNowPlayingInfoPropertyElapsedPlaybackTime] = NSTimeInterval(self.timeIntoSongPaused)
-		
+
 		MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo = info
 	}
-	
+
 	func setLockScreenPlayed() {
 		var dict = MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo
 		if (dict == nil) {
 			dict = [String: AnyObject]()
 		}
 		var info = dict!
-		
+
 		info[MPNowPlayingInfoPropertyPlaybackRate] = 1.0
 		info[MPNowPlayingInfoPropertyElapsedPlaybackTime] = NSTimeInterval(self.timeIntoSongPaused)
-		
+
 		MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo = info
 	}
-	
-	
-	
+
 	func setLockScreenData() {
 		let track = self.getCurrentSong()
 		let artists = track.artists
 		var artist: SPTPartialArtist
-		
+
 		if (artists.count > 0) {
 			artist = (artists[0] as! SPTPartialArtist)
 			SPTArtist.artistWithURI(artist.uri, session: self.session, callback: { (error, object) -> Void in
@@ -126,14 +145,14 @@ class PlayController: NSObject, SPTAudioStreamingDelegate{
 								dict = [String: AnyObject]()
 							}
 							var info = dict!
-							
+
 							info[MPMediaItemPropertyTitle] = track.name
 							info[MPMediaItemPropertyPlaybackDuration] = track.duration
 							info[MPMediaItemPropertyArtist] = artist.name
 							info[MPMediaItemPropertyAlbumArtist] = artist.name
 							info[MPMediaItemPropertyAlbumTitle] = track.album.name
 							info[MPNowPlayingInfoPropertyElapsedPlaybackTime] = self.timeIntoSongPaused
-							
+
 							MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo = info
 						})
 					}
@@ -142,54 +161,69 @@ class PlayController: NSObject, SPTAudioStreamingDelegate{
 		}
 	}
 
-	
-	
-	func beginPlaying(beenPlaying: Bool){
-		
-		if newPlaylistURI != currentPlaylistURI{
+	func beginPlaying(beenPlaying: Bool) {
+
+		if newPlaylistURI != currentPlaylistURI || didShuffle == true {
 			uris?.removeAll()
 		}
 		currentPlaylistURI = newPlaylistURI
 		for track in songs! {
 			uris?.append(track.uri)
 		}
-		
-		if  beenPlaying == true{
-	
+
+		if beenPlaying == true {
+
 			self.setLockScreenData()
 			self.setLockScreenPlayed()
-		
-			
-			self.setLockScreenData()
-			
 
+			self.setLockScreenData()
 		}
 		player?.playURIs(uris, fromIndex: Int32(currentSong), callback: nil)
 		self.timeIntoSongPaused = self.player!.currentPlaybackPosition
 	}
-	func next(){
+	func next() {
 		currentSong = currentSong + 1
 		beginPlaying(true)
-		
 	}
-	func previous(){
+	func previous() {
 		currentSong = currentSong - 1
 		beginPlaying(true)
-		
 	}
-	func pause(){
-		if player?.isPlaying == true{
+	func pause() {
+		if player?.isPlaying == true {
 			self.timeIntoSongPaused = self.player!.currentPlaybackPosition
 			player?.setIsPlaying(false, callback: nil)
 			self.setLockScreenPaused()
 		}
 	}
-	func play(){
-		if player?.isPlaying == false{
+	func play() {
+		if player?.isPlaying == false {
 			player?.setIsPlaying(true, callback: nil)
-		   self.timeIntoSongPaused = self.player!.currentPlaybackPosition
+			self.timeIntoSongPaused = self.player!.currentPlaybackPosition
 			self.setLockScreenPlayed()
 		}
-		
+	}
+}
+
+extension CollectionType {
+	/// Return a copy of `self` with its elements shuffled
+	func shuffle() -> [Generator.Element] {
+		var list = Array(self)
+		list.shuffleInPlace()
+		return list
+	}
+}
+
+extension MutableCollectionType where Index == Int {
+	/// Shuffle the elements of `self` in-place.
+	mutating func shuffleInPlace() {
+		// empty and single-element collections don't shuffle
+		if count < 2 { return }
+
+		for i in 0 ..< count - 1 {
+			let j = Int(arc4random_uniform(UInt32(count - i))) + i
+			guard i != j else { continue }
+			swap(&self[i], &self[j])
+		}
 	}
 }
--- Simpilfy/SongSelectionViewController.swift
+++ Simpilfy/SongSelectionViewController.swift
@@ -9,8 +9,8 @@
 import Foundation
 import UIKit
 
-class SongSelectionViewController: UITableViewController, SPTAudioStreamingDelegate{
-	
+class SongSelectionViewController: UITableViewController, SPTAudioStreamingDelegate {
+
 	var playlist: SPTPlaylistSnapshot!
 	var session: SPTSession!
 	var partialPlaylist: SPTPartialPlaylist!
@@ -18,30 +18,25 @@ class SongSelectionViewController: UITableViewController, SPTAudioStreamingDeleg
 	var allTracks: [SPTPartialTrack]? = []
 	var playerIntitalized = false
 	var manager: PlayController?
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		auth = SPTAuth.defaultInstance()
 		self.setupAuthorization()
 		manager = (UIApplication.sharedApplication().delegate as! AppDelegate).playController
-		
 	}
-	
+
 	func setupAuthorization() {
 		let homeViewController = self.parentViewController?.parentViewController?.childViewControllers[0].childViewControllers[0] as! HomeViewController
-		
+
 		self.session = homeViewController.session
-		
-		
+
 		auth!.clientID = "7fedf5f10ea84f069aae21eb9e06b73b"
 		auth!.redirectURL = NSURL(string: "simplyfy://login")
 		auth!.requestedScopes = [SPTAuthStreamingScope]
 		self.getPlaylistSnapshot()
-		
 	}
 
-	
-	
 	func getPlaylistSnapshot() -> Void {
 		if (self.session != nil) && (self.partialPlaylist != nil) {
 			SPTPlaylistSnapshot.playlistWithURI(self.partialPlaylist.playableUri, session: session, callback: { (error, snapshot) -> Void in
@@ -50,9 +45,9 @@ class SongSelectionViewController: UITableViewController, SPTAudioStreamingDeleg
 				} else {
 					print("fetched tracks")
 					self.playlist = snapshot as! SPTPlaylistSnapshot
-			
+
 					self.getTracks(self.session, trackList: self.playlist.firstTrackPage, callback: { tracks in
-						for track in tracks{
+						for track in tracks {
 							self.allTracks?.append(track)
 						}
 						self.tableView.reloadData()
@@ -61,13 +56,11 @@ class SongSelectionViewController: UITableViewController, SPTAudioStreamingDeleg
 			})
 		}
 	}
-	
 
-	
 	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
 		return 1
 	}
-	
+
 	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		if self.playlist != nil {
 			return (self.allTracks?.count)!
@@ -75,9 +68,7 @@ class SongSelectionViewController: UITableViewController, SPTAudioStreamingDeleg
 			return 0
 		}
 	}
-	
 
-	
 	func getTracks(session: SPTSession, trackList: SPTListPage, callback: (Array<SPTPartialTrack> -> Void)) {
 		// recursive case, if has next page, get it, then call this with a callback that assumes that this returns all tracks, and then callback again so that function that called this can get all tracks that this has
 		if (trackList.hasNextPage) {
@@ -85,7 +76,7 @@ class SongSelectionViewController: UITableViewController, SPTAudioStreamingDeleg
 				if let l = list as? SPTListPage {
 					self.getTracks(session, trackList: l, callback: { tracks in
 						var t = tracks
-						for  i in 0..<trackList.items.count{
+						for i in 0 ..< trackList.items.count {
 							t.append(trackList.items[i] as! SPTPartialTrack)
 						}
 						callback(t)
@@ -94,42 +85,56 @@ class SongSelectionViewController: UITableViewController, SPTAudioStreamingDeleg
 			})
 		} else { // base case, just get all the tracks, then callback so function that called gets all tracks
 			var tracks = Array<SPTPartialTrack>()
-			for i in 0..<trackList.items.count{
+			for i in 0 ..< trackList.items.count {
 				tracks.append(trackList.items[i] as! SPTPartialTrack)
 			}
 			callback(tracks)
 		}
 	}
+	@IBAction func shuffle() {
+		if playerIntitalized == false {
+			playerIntitalized = true
+			manager?.newPlaylistURI = partialPlaylist.uri
+			manager?.songs = allTracks
+			manager?.currentSong = randomInt(0, max: (allTracks?.count)!)
+			manager?.playShuffle()
+		} else {
+			manager?.newPlaylistURI = partialPlaylist.uri
+			manager?.songs = allTracks
+			manager?.currentSong = randomInt(0, max: (allTracks?.count)!)
+			manager?.playShuffle()
+		}
+	}
+	func randomInt(min: Int, max: Int) -> Int {
+		return min + Int(arc4random_uniform(UInt32(max - min + 1)))
+	}
 	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
 		self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
 		print(allTracks![indexPath.row].uri)
-		if playerIntitalized == false{
+
+		if playerIntitalized == false {
 			playerIntitalized = true
 			manager?.newPlaylistURI = partialPlaylist.uri
 			manager?.songs = allTracks
 			manager?.currentSong = indexPath.row
-		  manager?.initWithSongs()
-		}else{
+			manager?.initWithSongs()
+		} else {
+			manager?.player?.setIsPlaying(false, callback: nil)
 			manager?.newPlaylistURI = partialPlaylist.uri
 			manager?.songs = allTracks
 			manager?.currentSong = indexPath.row
 			manager?.beginPlaying(true)
 		}
-
-	
 	}
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier("Track", forIndexPath: indexPath)
-		
+
 		if self.playlist != nil {
-			
-				let track = allTracks![indexPath.row] 
-				cell.textLabel!.text = track.name
+
+			let track = allTracks![indexPath.row]
+			cell.textLabel!.text = track.name
 		}
 
-		
 		return cell
 	}
-	
-	
 }
